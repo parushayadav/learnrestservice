a6b10c080692cd25637ce6407a51b240371f6853
Chidvilas Parusha <parushacv@gmail.com>
2024-10-22 15:06:11 -0400

    initial commit

---
+++ src/main/java/com/manticore/learnrestservice/LearnrestserviceApplication.java
@@ -0,0 +1,13 @@
+package com.manticore.learnrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnrestserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnrestserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manticore/learnrestservice/LearnrestserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manticore.learnrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnrestserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
